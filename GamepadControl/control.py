c4059dc8f8fc4d799f055cb65467b1ab6f666907
varicoque <alvaro.morone@estudiante.uam.es>
2019-11-09 00:32:47 +0100

    taladradora incluida

--- GamepadControl/control.py
+++ GamepadControl/control.py
@@ -5,24 +5,21 @@ import math
 UDP_IP = '192.168.4.1'
 UDP_PORT = 42069
 
-BOOST_BUTTONS = [4, 5, 6, 7]
+BOOST_BUTTONS = [4, 5, 6, 7] #R1,L1,Start,Select
+TUNELADORA_ON = [0] #A
+TUNELADORA_OFF = [1] #B
 
 pygame.init()
-direction = gas = value = boost = 0
+direction = gas = value = boost = tuneladora = 0
 running = True
 
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 for joy in joysticks:
   joy.init()
 
-pygame.mixer.init()
-pygame.mixer.music.load("../Audio/SocavadorTrimmed.mp3")
-pygame.mixer.music.play()
-
-
 def send_to_robot(command):
   byts = list(map(lambda n: n.to_bytes(1, byteorder='big', signed=True), command))
-  #print(byts)
+  print(byts)
   sock = socket.socket(socket.AF_INET, # Internet
                        socket.SOCK_DGRAM) # UDP
   sock.sendto(b''.join(byts), (UDP_IP, UDP_PORT))
@@ -40,18 +37,24 @@ while running:
       value = int(event_values['value']) # Value range: [0-1] (float)
       if axis == 1:
         gas = -value
+ 	 
       elif axis == 3:
         direction = value
     elif event.type == pygame.JOYBUTTONDOWN:
       button = event_values['button']
       if button in BOOST_BUTTONS:
         boost = 1
+      elif button in TUNELADORA_ON:
+        tuneladora = 1
+      elif button in TUNELADORA_OFF:
+        tuneladora = 0
     elif event.type == pygame.JOYBUTTONUP:
       button = event_values['button']
       if button in BOOST_BUTTONS:
         boost = 0
 
+
     # UDP packet content: [+-Gas][+-Direction][Boost]
-    command = [gas, direction, boost]
+    command = [gas, direction, boost, tuneladora]
 
     send_to_robot(command)
